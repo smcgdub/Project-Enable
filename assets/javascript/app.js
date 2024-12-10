7ff6a2c88d6c30f6d05918c510b0d33e4ef1c408
Stephen <73615623+smcgdub@users.noreply.github.com>
2024-12-10 17:57:45 +0000

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Set the text of the div with id "year" to the current year
+document.getElementById('year').textContent = currentYear;
